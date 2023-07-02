a3d98ed3d8af0faf22eb243f3368a952f3faafc3
Fredrik Tvingstedt <Fredrik.Tvingstedt@alfaecare.se>
2023-07-02 15:58:13 +0200

    update

--- src/components/home/MenuCard.jsx
+++ src/components/home/MenuCard.jsx
@@ -21,16 +21,14 @@ const MenuCard = ({ itemNum, burgerSrc, price, title, info, cal, handler, delay 
       <div></div>
       <main>
         <img src={burgerSrc} alt={itemNum} />
-
         <h5>${price}</h5>
-        
         <p>{title}</p>
         <p>{info}</p>
         <p>{cal}Cal.</p>
         <Popup trigger=
           {<button onClick={() => handler(itemNum)}>Buy Now</button>}
             >
-              <div style={{color:"red", transform: 'translate(0%,-500%)', backgroundColor: '#fff', padding: '10px', borderRadius: '5px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)'}}>
+              <div style={{color:"black", transform: 'translate(0%,-500%)', backgroundColor: '#fff', padding: '10px', borderRadius: '5px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)'}}>
                 Added to cart!
               </div>
         </Popup>
