ca4cb9b11bbea84881f1b5e959a880efea8abf62
Fredrik Tvingstedt <Fredrik.Tvingstedt@alfaecare.se>
2023-07-03 14:47:57 +0200

    working smtp

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,57 +1,58 @@
 import React from "react";
 import { motion } from "framer-motion";
+import emailjs from "emailjs-com";
 import Popup from 'reactjs-popup';
 
-const Contact = () => { return (
+const Contact = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
 
+    // Configure email parameters
+    const emailParams = {
+      to_email: "burgershop@dotnet.nu",
+      to_name: "Admin",
+      from_name: "userName",
+      from_email: "userEmail@exempel.com",
+      message: "This is the email message.",
+    };
 
+    // Send email using emailjs
+    emailjs.send(
+      "service_ni3i1er",
+      "template_txjhaon",
+      emailParams,
+      "lxgOFrVQtj196ZPra"
+    )
+    .then((response) => {
+      console.log("Email sent:", response.status);
+    })
+    .catch((error) => {
+      console.error("Error sending email:", error);
+    });
+  };
 
-    
+  return (
     <section className="contact">
-        <motion.form initial={{
-                x: "-100vw",
-                opacity: 0,
-            }}
-                animate={{ x: 0,
-                opacity: 1,
-            }}
-                transition={{ delay: 0.2 }}
-        >
-
-                <h2>Contact Us</h2>
-                <input type="text" placeholder="Name" />
-                <input type="email" placeholder="Email" />
-                <textarea placeholder="Message..." cols="30" rows="10"></textarea>
-            
-                <Popup trigger=
-                    {<button type="button">Send</button>}
-                >
-                    <div style={{
-                        color:"red", 
-                        transform: 'translate(25%,-700%)',
-                        backgroundColor: '#fff',
-                        padding: '10px',
-                        borderRadius: '5px', 
-                        boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)'}}>Thank you for cotacting us! We will get back to you shortly.
-                    </div>
-                </Popup>
-        </motion.form>
-    
-    
-        <motion.div 
-            className="formBorder" initial={{
-                x: "100vw",
-                opacity: 0,
-            }}
-                animate={{ x: 0,
-                opacity: 1,
-                }}
-                
-                transition={{ delay: 0.2 }}
-            >
-
-        </motion.div>
+      <motion.form
+        initial={{
+          x: "-100vw",
+          opacity: 0,
+        }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ delay: 0.3 }}
+      >
+   
+        <h2>Contact Us</h2>
+        <input type="text" placeholder="Your Name" />
+        <input type="email" placeholder="Your Email" />
+        <textarea placeholder="Your Message..." cols="30" rows="20"></textarea>
+        <button type="submit" onClick={sendEmail}>Send</button>
+       
+      </motion.form>
     </section>
-);
+  );
 };
+
 export default Contact;
+
+
