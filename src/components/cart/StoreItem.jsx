bcf5f999fb714a01be06395d5585808a4858ae0e
Fredrik Tvingstedt <Fredrik.Tvingstedt@alfaecare.se>
2023-07-13 14:10:55 +0200

    update

--- src/components/cart/StoreItem.jsx
+++ src/components/cart/StoreItem.jsx
@@ -1,6 +1,9 @@
-import { Button, Card } from "react-bootstrap";
-import { useShoppingCart } from "../../context/ShoppingCartContext";
-import { formatCurrency } from "../../utilities/formatCurrency";
+import React, { useState } from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { useShoppingCart } from '../../context/ShoppingCartContext';
+import { formatCurrency } from '../../utilities/formatCurrency';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 export function StoreItem({ id, name, price, imgUrl }) {
   const {
@@ -11,6 +14,13 @@ export function StoreItem({ id, name, price, imgUrl }) {
   } = useShoppingCart();
   const quantity = getItemQuantity(id);
 
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleBuyNow = () => {
+    increaseCartQuantity(id);
+    setTimeout(() => setShowPopup(true), 100); // Delayed appearance of popup after 0,1 second
+  };
+
   return (
     <Card className="h-100">
       <div id="storeItem-top"></div>
@@ -18,7 +28,7 @@ export function StoreItem({ id, name, price, imgUrl }) {
         variant="top"
         src={imgUrl}
         height="250px"
-        style={{ objectFit: "cover" }}
+        style={{ objectFit: 'cover' }}
       />
       <Card.Body className="d-flex flex-column">
         <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
@@ -27,23 +37,37 @@ export function StoreItem({ id, name, price, imgUrl }) {
         </Card.Title>
         <div className="mt-auto">
           {quantity === 0 ? (
-            <Button className="w-100" onClick={() => increaseCartQuantity(id)} style={{ backgroundColor: 'rgb(156, 0, 60)' }}>
+            <Button
+              className="w-100"
+              onClick={handleBuyNow}
+              style={{ backgroundColor: 'rgb(156, 0, 60)' }}
+            >
               Buy Now
             </Button>
           ) : (
             <div
               className="d-flex align-items-center flex-column"
-              style={{ gap: ".5rem" }}
+              style={{ gap: '.5rem' }}
             >
               <div
                 className="d-flex align-items-center justify-content-center"
-                style={{ gap: ".5rem" }}
+                style={{ gap: '.5rem' }}
               >
-                <Button onClick={() => decreaseCartQuantity(id)} style={{ backgroundColor: 'black' }} >-</Button>
+                <Button
+                  onClick={() => decreaseCartQuantity(id)}
+                  style={{ backgroundColor: 'black' }}
+                >
+                  -
+                </Button>
                 <div>
                   <span className="fs-3">{quantity}</span> in cart
                 </div>
-                <Button onClick={() => increaseCartQuantity(id)} style={{ backgroundColor: 'black' }} >+</Button>
+                <Button
+                  onClick={() => increaseCartQuantity(id)}
+                  style={{ backgroundColor: 'black' }}
+                >
+                  +
+                </Button>
               </div>
               <Button
                 onClick={() => removeFromCart(id)}
@@ -57,6 +81,24 @@ export function StoreItem({ id, name, price, imgUrl }) {
           )}
         </div>
       </Card.Body>
+      {showPopup && (
+        <Popup
+          open={showPopup}
+          onClose={() => setShowPopup(false)}
+          modal
+          closeOnDocumentClick
+          contentStyle={{
+            backgroundColor: 'white',
+            color: 'black',
+            padding: '15px',
+            borderRadius: '5px',
+            boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+            width: '135px',
+          }}
+        >
+          <span>Added to cart!</span>
+        </Popup>
+      )}
     </Card>
   );
 }
