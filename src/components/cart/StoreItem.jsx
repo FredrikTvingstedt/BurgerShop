814a97ac1a82d49a5aac2f5e99169d954d1fa367
Fredrik Tvingstedt <Fredrik.Tvingstedt@alfaecare.se>
2023-07-13 14:37:44 +0200

    update

--- src/components/cart/StoreItem.jsx
+++ src/components/cart/StoreItem.jsx
@@ -45,7 +45,7 @@ export function StoreItem({ id, name, price, imgUrl, delay }) {
         <Card.Img
           variant="top"
           src={imgUrl}
-          height="250px"
+          height="300px"
           style={{ objectFit: 'cover' }}
         />
         <Card.Body className="d-flex flex-column">
