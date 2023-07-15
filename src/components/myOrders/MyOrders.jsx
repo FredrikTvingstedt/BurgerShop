315c8883d95180cdcd8ac6e2995024a2f11a0707
Fredrik Tvingstedt <Fredrik.Tvingstedt@alfaecare.se>
2023-07-15 13:32:05 +0200

    update

--- src/components/myOrders/MyOrders.jsx
+++ src/components/myOrders/MyOrders.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { AiOutlineEye } from "react-icons/ai";
+import { useShoppingCart } from "../../context/ShoppingCartContext";
+import { formatCurrency } from "../../utilities/formatCurrency";
+import storeItems from "../../data/items.json";
 
 const MyOrders = () => {
-  const arr = [1];
+  const { cartItems } = useShoppingCart();
+
+  // Calculate the total sum for all items in the cart
+  const totalSum = cartItems.reduce((total, cartItem) => {
+    const item = storeItems.find((i) => i.id === cartItem.id);
+    return total + (item?.price || 0) * cartItem.quantity;
+  }, 0);
+
+  // Calculate the total quantity of items in the cart
+  const totalQuantity = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
 
   return (
     <section className="tableClass">
@@ -14,28 +26,26 @@ const MyOrders = () => {
               <th>Order Id</th>
               <th>Status</th>
               <th>Item Qty</th>
-              <th>Amount</th>
+              <th>Sub Total</th>
               <th>Payment Method</th>
               <th>Order Details</th>
             </tr>
           </thead>
 
           <tbody>
-            {arr.map((i) => (
-            <tr key={i}>
-            <td>#00001</td>
-            <td>Processing</td>
-            <td>Add Info</td>
-            <td>Add Info</td>
-            <td>COD</td>
-            <td>
-            <Link to={`/order/${"00001"}`}>
-            <AiOutlineEye />
-            </Link>
-            </td>
+            <tr>
+              <td>#00001</td>
+              <td>Processing</td>
+              <td>{totalQuantity}</td>
+              <td>{formatCurrency(totalSum)}</td>
+              <td>COD</td>
+              <td>
+                <Link to={`/order/${"00001"}`}>
+                  <AiOutlineEye />
+                </Link>
+              </td>
             </tr>
-            ))}
-            </tbody>
+          </tbody>
         </table>
       </main>
     </section>
